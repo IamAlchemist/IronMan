0b22f89d1f432f842a8047761675bfbb4fdad37c
IamAlchemist <Wizard.Lee@qq.com>
2016-10-13 09:22:18 +0800

    download the page of algebra-home

---
+++ spider/spider.js
@@ -0,0 +1,18 @@
+/**
+ * Created by wizard on 10/13/16.
+ */
+
+const https = require('https');
+
+https.get('https://www.khanacademy.org/math/algebra-home', (res) => {
+    console.log('statusCode: ', res.statusCode);
+console.log('headers: ', res.headers);
+
+res.on('data', (d) => {
+    process.stdout.write(d);
+});
+
+}).on('error', (e) => {
+    console.error(e);
+});
+
