d0ef8a36604db8f433321ebe7c6bb784d7170325
IamAlchemist <Wizard.Lee@qq.com>
2016-10-21 19:23:32 +0800

    mc

--- ironman/routes/index.js
+++ ironman/routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+    var book = {
+        poster: "poster",
+        title: "title",
+        buyUrl: "buyUrl"
+    };
+    var books = new Array(3).fill(book);
+
+
+    var options = {
+        title: 'Express',
+        books: books
+    };
+
+    res.render('index', options);
 });
 
 module.exports = router;
