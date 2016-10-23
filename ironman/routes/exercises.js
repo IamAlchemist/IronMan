0ae4c5cbb89d7c97868516a791cae11a46850df2
IamAlchemist <Wizard.Lee@qq.com>
2016-10-23 15:46:02 +0800

    mc

---
+++ ironman/routes/exercises.js
@@ -0,0 +1,32 @@
+/**
+ * Created by wizard on 10/23/16.
+ */
+
+const express = require('express');
+const router = express.Router();
+const mongoose = require('../libs/mongoose');
+
+router.get('/', function (req, res, next) {
+    var options = new Array(3);
+
+    for (var i = 0; i < 3; ++i) {
+        options[i] = {
+            id: `option_${i}`,
+            name: 'option_name',
+            description: 'option_description'
+        };
+    }
+    var exercise = {
+        question: 'exer_question',
+        description: 'exer_description',
+        options
+    };
+
+    var params = {
+        exercise
+    };
+
+    res.render('learn', params);
+});
+
+module.exports = router;
