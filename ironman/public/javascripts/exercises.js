7bd63c7e3f0b400606750824572412ff8c2fbe7d
IamAlchemist <Wizard.Lee@qq.com>
2016-10-23 09:32:56 +0800

    mc

---
+++ ironman/public/javascripts/exercises.js
@@ -0,0 +1,15 @@
+/**
+ * Created by wizard on 10/23/16.
+ */
+
+$(document).ready(function () {
+    var inputs = $('input');
+    inputs.iCheck({
+        radioClass: 'iradio_square-green',
+        increaseArea: '20%'
+    });
+
+    inputs.on('ifChecked', event => {
+        alert(event.type + ' callback');
+    });
+});
