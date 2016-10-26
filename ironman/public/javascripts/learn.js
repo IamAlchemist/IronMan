480fc8dd839ff09440917e89586cad6e243daf79
IamAlchemist <Wizard.Lee@qq.com>
2016-10-26 08:44:30 +0800

    mc

--- ironman/public/javascripts/learn.js
+++ ironman/public/javascripts/learn.js
@@ -2,19 +2,22 @@
  * Created by wizard on 10/26/16.
  */
 
-$(document).ready(function () {
-    $('form#add').submit(function (event) {
-        event.preventDefault();
-        var form = $(this);
-        console.log(form.serialize());
-        $.ajax({
-            type: form.attr('method'),
-            url: form.attr('action'),
-            data: form.serialize()
-        }).done(function () {
-            alert('ok');
-        }).fail(function (jqXHR, textStatus, errorThrown) {
-            alert('fail');
+require([], function (){
+    $(document).ready(function () {
+        $('form#add').submit(function (event) {
+            event.preventDefault();
+            var form = $(this);
+            console.log(form.serialize());
+            $.ajax({
+                type: form.attr('method'),
+                url: form.attr('action'),
+                data: form.serialize()
+            }).done(function () {
+                alert('ok');
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                alert('fail');
+            });
         });
     });
 });
+
