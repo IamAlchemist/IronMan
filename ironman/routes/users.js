1885f23b3873932fa93dc689be5a8890b03e4ef6
IamAlchemist <Wizard.Lee@qq.com>
2016-10-28 09:34:52 +0800

    mc

--- ironman/routes/users.js
+++ ironman/routes/users.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.send('respond with a resource');
 });
 
-router.get('/login', checkAuthentication);
-router.get('/login', function (req, res, next) {
+router.get('/login', noCheckAuthentication);
+router.get('/login', function (req, res) {
     var options = {
         title: 'IronMan Login'
     };
@@ -18,7 +18,7 @@ router.get('/login', function (req, res, next) {
     res.render('login', options);
 });
 
-router.post('/login', checkAuthentication);
+router.post('/login', noCheckAuthentication);
 router.post("/login", function (req, res) {
     var name = req.body.username,
         password = req.body.password,
@@ -26,28 +26,29 @@ router.post("/login", function (req, res) {
         md5_password = md5.update(password).digest('hex');
     if (name == "" || password == "") {
         req.session.error = "请不要留白！";
-        return res.redirect('/login');
+        return res.redirect('/users/login');
     }
 
     User.get(name, function(err, user) {
         if (!user) {
             req.session.error = "用户不存在！";
-            return res.redirect('/login');
+            return res.redirect('/users/login');
         }
 
         if (user.password != md5_password) {
             req.session.error = "密码错误！";
-            return res.redirect('/login');
+            return res.redirect('/users/login');
         }
 
         req.session.user = user;
         req.session.success = "登录成功！";
 
-        res.redirect('/home');
+        res.redirect('/users/profile');
     });
 });
 
-router.get('/register', function (req, res, next) {
+router.get('/register', noCheckAuthentication);
+router.get('/register', function (req, res) {
     var options = {
         title: 'IronMan Register'
     };
@@ -55,7 +56,52 @@ router.get('/register', function (req, res, next) {
     res.render('register', options);
 });
 
-router.get('/profile', function (req, res, next) {
+router.post("/register", noCheckAuthentication);
+router.post("/register", function (req, res) {
+
+    var name = req.body.username,
+        password = req.body.password,
+        repassword = req.body['repassword'];
+
+    if (name == "" || password == "" || repassword == "") {
+        req.session.error = "请不要留白！";
+        return res.redirect('/users/register');
+    }
+
+    if (password != repassword) {
+        req.session.error = "两次密码输入不一样";
+        return res.redirect('/users/register');
+    }
+
+    var md5 = crypto.createHash('md5'),
+        passwd = md5.update(req.body.password).digest('hex');
+    var newUser = new User({
+        name: name,
+        password: passwd
+    });
+
+    User.get(newUser.name, function (err, user) {
+        if (user) {
+            req.session.error = "用户已经存在!";
+            return res.redirect('/users/register');
+        }
+
+        newUser.save(function (err, user) {
+            if (err) {
+                req.session.error = err;
+                return res.redirect('/users/register');
+            }
+
+            req.session.user = user;
+            req.session.success = "注册成功！";
+            res.redirect('/users/profile');
+        });
+
+    });
+});
+
+router.get('/profile', checkAuthentication);
+router.get('/profile', function (req, res) {
     var options = {
         title: 'IronMan Profile'
     };
@@ -63,14 +109,15 @@ router.get('/profile', function (req, res, next) {
     res.render('profile', options);
 });
 
-function authentication (req, res, next) {
+function checkAuthentication (req, res, next) {
     if (!req.session.user) {
         req.session.error = '请登录';
         return res.redirect('/users/login');
     }
     next();
 }
-function checkAuthentication (req, res, next) {
+
+function noCheckAuthentication (req, res, next) {
     if (req.session.user) {
         req.session.error = '已登录';
         return res.redirect('/users/profile');
