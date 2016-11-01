1de4f613927df3300fa762bdfaac6e4ece619812
IamAlchemist <Wizard.Lee@qq.com>
2016-11-01 13:55:07 +0800

    minor refactor

--- ironman/public/javascripts/exercises/create.js
+++ ironman/public/javascripts/exercises/create.js
@@ -2,38 +2,17 @@
  * Created by wizard on 10/26/16.
  */
 
-require([], function () {
+require(['../libs/ironmanLib'], function (ironmanLib) {
     var messageElem;
 
     $(document).ready(function () {
         "use strict";
         messageElem = $('p#message');
 
-        setupCreateForm();
-    });
-
-    function setupCreateForm() {
-        const createForm = $('form#create');
-
-        createForm.submit(function (event) {
-            event.preventDefault();
-
-            const form = $(this);
-
-            $.ajax({
-                type: form.attr('method'),
-                url: form.attr('action'),
-                data: form.serialize(),
-            }).done(function (data) {
-                let msg = data.errorCode == 0 ? data.content.message : data.message;
-                messageElem.text(msg);
-                if (data.errorCode == 0) {
-                    createForm[0].reset();
-                }
-                else if (data.errorCode == 8) {
-                    window.location.replace('/users/login');
-                }
-            })
+        ironmanLib.setupCreateForm($('form#create'), function (data) {
+            let msg = data.content != undefined ? data.content.message : undefined;
+            msg = msg == undefined ? data.message : msg;
+            messageElem.text(msg);
         });
-    }
+    });
 });
