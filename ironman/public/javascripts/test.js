d0cea2d33fa6929ee9efde434ab3134d661a9f02
IamAlchemist <Wizard.Lee@qq.com>
2016-11-02 13:46:10 +0800

    mc

--- ironman/public/javascripts/test.js
+++ ironman/public/javascripts/test.js
@@ -5,10 +5,46 @@
 require([], function () {
     $(document).ready(function () {
         var source   = $("#entry-template").html();
-        var template = Handlebars.compile(source);
-        var context = {title: "My New Post", body: "This is my first post!"};
-        var html    = template(context);
+        var template1 = Handlebars.compile(source);
+        var context1 = {title: "My New Post", body: "This is my first post!"};
+        var html1 = template1(context1);
 
-        $('#content').html(html);
+        var listContext =
+        {
+            people: [
+                {firstName: "Yehuda", lastName: "Katz"},
+                {firstName: "Carl", lastName: "Lerche"},
+                {firstName: "Alan", lastName: "Johnson"}
+            ]
+        };
+
+        Handlebars.registerHelper('list', function(items, options) {
+            var out = "<ul>";
+
+            for(var i=0, l=items.length; i<l; i++) {
+                out = out + "<li>" + options.fn(items[i]) + "</li>";
+            }
+
+            return out + "</ul>";
+        });
+
+        var source2   = $("#list-template").html();
+        var template2 = Handlebars.compile(source2);
+        var html2 = template2(listContext);
+
+        var pathContext = {
+            title: "My First Blog Post!",
+            author: {
+                id: 47,
+                name: "Yehuda Katz"
+            },
+            body: "My first post. Wheeeee!"
+        };
+
+        var source3   = $("#path-template").html();
+        var template3 = Handlebars.compile(source3);
+        var html3 = template3(pathContext);
+
+        $('#content').html(html3);
     });
 });
