557fe7afe6b9edf269de923f160c1e55cb99b197
IamAlchemist <Wizard.Lee@qq.com>
2016-11-02 23:33:56 +0800

    add time stamp in word exercises

--- ironman/models/wordExercise.js
+++ ironman/models/wordExercise.js
@@ -5,8 +5,10 @@
 'use strict';
 
 const mongodb = require('../libs/mongodb');
+const timestamps = require('../libs/mongoose-timestamp');
+const Schema = mongodb.Schema;
 
- const WordExerciseModel = mongodb.model('WordExercise', {
+const WordExerciseSchema = new Schema({
     mail: String,
     word: String,
     partOfSpeech: String,
@@ -15,25 +17,29 @@ const mongodb = require('../libs/mongodb');
     exampleExplanation: String,
     others: String
 });
+WordExerciseSchema.plugin(timestamps);
+
+const WordExerciseModel = mongodb.model('WordExercise', WordExerciseSchema);
 module.exports.WordExerciseModel = WordExerciseModel;
 
-module.exports.MakeWordExercise = function MakeWordExercise(mail = throwIfMissing(),
-                                 word = throwIfMissing(),
-                                 partOfSpeech = throwIfMissing(),
-                                 explanation = throwIfMissing(),
-                                 example = throwIfMissing(),
-                                 exampleExplanation = throwIfMissing(),
-                                 others = '')
-{
-    return new WordExerciseModel({
-        mail,
-        word,
-        partOfSpeech,
-        explanation,
-        example,
-        exampleExplanation,
-        others});
-};
+module.exports.MakeWordExercise =
+    function MakeWordExercise(mail = throwIfMissing(),
+                              word = throwIfMissing(),
+                              partOfSpeech = throwIfMissing(),
+                              explanation = throwIfMissing(),
+                              example = throwIfMissing(),
+                              exampleExplanation = throwIfMissing(),
+                              others = '') {
+        return new WordExerciseModel({
+            mail,
+            word,
+            partOfSpeech,
+            explanation,
+            example,
+            exampleExplanation,
+            others
+        });
+    };
 
 function throwIfMissing() {
     throw new Error('Missing parameter');
--- ironman/models/wordExerciseProgress.js
+++ ironman/models/wordExerciseProgress.js
@@ -3,22 +3,28 @@
  */
 
 const mongodb = require('../libs/mongodb');
+const timestamps = require('../libs/mongoose-timestamp');
+const Schema = mongodb.Schema;
 
-const WordExerciseProgressModel = mongodb.model('WordExerciseProgress', {
+const WordExerciseProgressSchema = new Schema({
     mail: String,
     wordExercise: Object,
     progress: Number
 });
+WordExerciseProgressSchema.plugin(timestamps);
 
+const WordExerciseProgressModel =
+    mongodb.model('WordExerciseProgress', WordExerciseProgressSchema);
 module.exports.WordExerciseProgressModel = WordExerciseProgressModel;
 
 function throwIfMissing() {
     throw new Error('Missing parameter');
 }
 
-module.exports.MakeWordExerciseProgress = function MakeWordExerciseProgress (mail = throwIfMissing(),
-                                          wordExercise= throwIfMissing(),
-                                          progress = 0) {
-    return new WordExerciseProgressModel({mail, wordExercise, progress})
-};
+module.exports.MakeWordExerciseProgress =
+    function MakeWordExerciseProgress(mail = throwIfMissing(),
+                                      wordExercise = throwIfMissing(),
+                                      progress = 0) {
+        return new WordExerciseProgressModel({mail, wordExercise, progress});
+    };
 
