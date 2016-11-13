23961174613e0bade552dfcf4a5f80ef4c82cd45
IamAlchemist <Wizard.Lee@qq.com>
2016-11-13 09:32:48 +0800

    change import db

--- ironman/playgrounds/importToDB.js
+++ ironman/playgrounds/importToDB.js
@@ -2,20 +2,34 @@
  * Created by wizard on 11/11/16.
  */
 
-const mongodb = require('../libs/mongodb'),
-    Promise = require('bluebird'),
+const Promise = require('bluebird'),
     fs = require('fs'),
+    program = require('commander'),
     WordExercise = require('../models/wordExercise'),
     logger = require('../libs/ironmanLogger');
 
+program
+    .version('0.0.1')
+    .option('-S, --source [file]', 'source Of words [file]', '')
+    .option('-m, --mail [mail]', 'source Of words [mail]', '')
+    .parse(process.argv);
 
-function import_eight_a() {
-    var words = JSON.parse(fs.readFileSync('./eight_grade_half_a.json', 'utf8'));
+console.log('import word exercises:');
+console.log(`from : ${program.source}`);
+console.log(`mail : ${program.mail}`);
+
+if (!program.source) {
+    console.log('need parameter');
+    process.exit(1);
+}
+
+function importWordExercises(jsonFile, mail) {
+    var words = JSON.parse(fs.readFileSync(`./${jsonFile}`, 'utf8'));
 
     logger.info(`count: ${words.length}`);
 
     words = words.map((word)=> {
-        word.mail = 'eight.grade.half.a@gmail.com';
+        word.mail = mail;
         return word;
     });
 
@@ -37,41 +51,9 @@ function import_eight_a() {
     const final = new Promise.all(promises);
 
     final.then((results)=> {
-        logger.info(`eight_grade_half_a succeed: ${results.length}`)
+        logger.info(`${jsonFile} succeed: ${results.length}`)
     });
 }
 
-function import_eight_b() {
-    var words = JSON.parse(fs.readFileSync('./eight_grade_half_b.json', 'utf8'));
+importWordExercises(program.source, program.mail);
 
-    logger.info(`count: ${words.length}`);
-
-    words = words.map((word)=> {
-        word.mail = 'eight.grade.half.b@gmail.com';
-        return word;
-    });
-
-    var promises = words.map((word)=> {
-
-        return WordExercise.MakeWordExercise(
-            word.mail,
-            word.word,
-            word.partOfSpeech,
-            word.explanation,
-            word.example,
-            word.exampleExplanation,
-            word.pronunciation,
-            word.others
-        ).save();
-
-    });
-
-    const final = new Promise.all(promises);
-
-    final.then((results)=> {
-        logger.info(`eight_grade_half_b succeed: ${results.length}`)
-    });
-}
-
-//import_eight_a();
-import_eight_b();
