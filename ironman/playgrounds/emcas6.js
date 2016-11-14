2a927e091c54f861a07f7694be8372c4477f0425
IamAlchemist <Wizard.Lee@qq.com>
2016-11-14 13:06:16 +0800

    mc

--- ironman/playgrounds/emcas6.js
+++ ironman/playgrounds/emcas6.js
@@ -2,9 +2,4 @@
  * Created by wizard on 11/2/16.
  */
 const moment = require('moment');
-
-var hello = {key1:'value'};
-
-for (let key of hello){
-    console.log(key);
-}
+console.log(moment().format('MMMM Do YYYY, HH:mm:ss'));
