055655bab42b978294d184e8c3b24bdea096b104
IamAlchemist <282539259@qq.com>
2016-12-06 14:34:00 +0800

    refactor

--- ironman/public/javascripts/exercises/punching.records.js
+++ ironman/public/javascripts/exercises/punching.records.js
@@ -1,7 +1,7 @@
 /**
  * Created by wizard on 12/6/16.
  */
-require(['../libs/ironmanLib', 'http://cdn.bootcss.com/moment.js/2.17.0/moment.min.js'], function (ironmanLib, moment) {
+require(['../libs/ironmanLib'], function (ironmanLib) {
     let messageAlertElem;
     let panelsElem;
     let panelsTmpl;
@@ -12,74 +12,6 @@ require(['../libs/ironmanLib', 'http://cdn.bootcss.com/moment.js/2.17.0/moment.m
         panelsTmpl = $('#calendars-template').html();
 
         let punchingType = panelsElem.attr("data");
-        showCalendarsWithType(punchingType);
+        ironmanLib.showCalendarsWithType(punchingType, panelsElem, panelsTmpl);
     });
-
-    function showCalendarsWithType(punchingType) {
-        let url = "";
-        if (punchingType == "homework") {
-            url = '/exercises/punching/records/homework';
-        }
-        else if (punchingType == "word") {
-            url = '/exercises/punching/records/word';
-        }
-
-        if (url.length != 0) {
-            showCalendarWithURL(url);
-        }
-    }
-
-    function showCalendarWithURL(url) {
-        $.getJSON(url)
-            .done((result) => {
-                if (result.errorCode == 0) {
-                    let arrayOfPunchings = result.content;
-                    if (arrayOfPunchings.length > 0) {
-                        showCalendars(arrayOfPunchings);
-                    }
-                }
-            })
-    }
-
-    function showCalendars(arrayOfPunchings) {
-        let template = Handlebars.compile(panelsTmpl);
-        let panelTitles = Array();
-        for (let item of arrayOfPunchings) {
-            if (item.length > 0) {
-                let punching = item[0];
-                let title = punching.mail;
-                panelTitles.push(title);
-            }
-        }
-        let html = template(panelTitles);
-        panelsElem.html(html);
-
-        const len = arrayOfPunchings.length;
-        for (let i = 0; i < len; ++i ) {
-            showACalendar(`#calendar_${i}`, arrayOfPunchings[i])
-        }
-    }
-
-    function showACalendar(calendarId, punchings) {
-        let createdAts = punchings.map(punching => punching.createdAt);
-        let momentStrings = createdAts.map(createdAt => moment(createdAt));
-
-        $(calendarId).calendar({
-            customDayRenderer: function (element, date) {
-                var calendarMoment = moment(date);
-                for (let str of momentStrings) {
-                    let m = moment(str);
-                    if (m.year() == calendarMoment.year()
-                        && m.month() == calendarMoment.month()
-                        && m.date() == calendarMoment.date()) {
-
-                        $(element).css('background-color', 'red');
-                        $(element).css('color', 'white');
-                        $(element).css('border-radius', '15px');
-                    }
-                }
-            }
-        });
-    }
-
 });
--- ironman/public/javascripts/libs/ironmanLib.js
+++ ironman/public/javascripts/libs/ironmanLib.js
@@ -2,7 +2,7 @@
  * Created by wizard on 11/1/16.
  */
 
-define(function () {
+define(['http://cdn.bootcss.com/moment.js/2.17.0/moment.min.js'], function (moment) {
     var add = function (x, y) {
         return x + y;
     };
@@ -65,12 +65,80 @@ define(function () {
             json.content.message : json.message;
     }
 
+    function showCalendarsWithType(punchingType, elem, templ) {
+        let url = "";
+        if (punchingType == "homework") {
+            url = '/exercises/punching/records/homework';
+        }
+        else if (punchingType == "word") {
+            url = '/exercises/punching/records/word';
+        }
+
+        if (url.length != 0) {
+            showCalendarWithURL(url, elem, templ);
+        }
+    }
+
+    function showCalendarWithURL(url, elem, templ) {
+        $.getJSON(url)
+            .done((result) => {
+                if (result.errorCode == 0) {
+                    let arrayOfPunchings = result.content;
+                    if (arrayOfPunchings.length > 0) {
+                        showCalendars(arrayOfPunchings, elem, templ);
+                    }
+                }
+            })
+    }
+
+    function showCalendars(arrayOfPunchings, elem, templ) {
+        let template = Handlebars.compile(templ);
+        let panelTitles = Array();
+        for (let item of arrayOfPunchings) {
+            if (item.length > 0) {
+                let punching = item[0];
+                let title = punching.mail;
+                panelTitles.push(title);
+            }
+        }
+        let html = template(panelTitles);
+        elem.html(html);
+
+        const len = arrayOfPunchings.length;
+        for (let i = 0; i < len; ++i ) {
+            showACalendar(`#calendar_${i}`, arrayOfPunchings[i])
+        }
+    }
+
+    function showACalendar(calendarId, punchings) {
+        let createdAts = punchings.map(punching => punching.createdAt);
+        let momentStrings = createdAts.map(createdAt => moment(createdAt));
+
+        $(calendarId).calendar({
+            customDayRenderer: function (element, date) {
+                var calendarMoment = moment(date);
+                for (let str of momentStrings) {
+                    let m = moment(str);
+                    if (m.year() == calendarMoment.year()
+                        && m.month() == calendarMoment.month()
+                        && m.date() == calendarMoment.date()) {
+
+                        $(element).css('background-color', 'red');
+                        $(element).css('color', 'white');
+                        $(element).css('border-radius', '15px');
+                    }
+                }
+            }
+        });
+    }
+
+
     return {
         add: add,
         alert: alert,
         setupForm: setupForm,
         decorateRadioInputs: decorateRadioInputs,
-        getMessageFromJson: getMessageFromJson
-
+        getMessageFromJson: getMessageFromJson,
+        showCalendarsWithType: showCalendarsWithType
     };
 });
